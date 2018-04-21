2eb9f31e28033f1d727ed2cf092d22109a0410ee
raghavan.mk <raghavan.mk@gmail.com>
2018-04-21 19:47:49 +0530

    added enumerators

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ namespace threads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("just adding a line so file is modified");            
-            Console.WriteLine("Git ssh test");
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("just adding a line so file is modified");            
+            // Console.WriteLine("Git ssh test");
+
+            Enumerators enums = new Enumerators();
+            foreach(int i in enums.GetEnumerator()){
+               Console.WriteLine(i);
+           }            
         }
     }
 }
